6af6f63176f20909b75a63b0c7182765c84d482f
JM <jujumont333acc@gmail.com>
2026-09-10 15:50:15 -0700

    fixed triangular and badge

--- Fixme.py
+++ Fixme.py
@@ -24,11 +24,6 @@ def factorial(n):
 
 def triangular(n):
     '''
-    Returns the nth triangular number.
-
-    The nth triangular number is the sum of all numbers from 1 to n.
-    It is like the factorial, but uses addition instead of multiplication.
-
     >>> triangular(1)
     1
     >>> triangular(2)
@@ -42,3 +37,7 @@ def triangular(n):
     >>> triangular(400)
     80200
     '''
+    result = 0
+    for i in range(1, n + 1):
+        result += i
+    return result
